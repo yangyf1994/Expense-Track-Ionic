64257c8d6755a6b9ea8bf9c25453e5e017bcb42c
Yifeng Yang <yang-yifeng@hotmail.com>
2015-07-28 15:08:23 +0700

    added in firebase support

--- expense-track/www/js/controllers.js
+++ expense-track/www/js/controllers.js
@@ -1,15 +1,12 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
-   $scope.expenses = [{
-        by: 'me',
-        label: 'test',
-        cost: 10
-    }];
+.controller('DashCtrl', function($scope,fireBaseData,$firebase) {
+   $scope.expenses = $firebase(fireBaseData.refExpenses()).$asArray();
+   $scope.user = fireBaseData.ref().getAuth();
 
     $scope.addExpense = function(e) {
-        $scope.expenses.push({
-            by: 'test',
+        $scope.expenses.$add({
+            by: $scope.user.password.email,
             label: $scope.label,
             cost: $scope.cost
         });
@@ -26,14 +23,33 @@ angular.module('starter.controllers', [])
     };
 })
 
-.controller('FriendsCtrl', function($scope) {
+.controller('FriendsCtrl', function($scope,fireBaseData, $firebase) {
+    $scope.user = fireBaseData.ref().getAuth();
+    $scope.expenses = $firebase(fireBaseData.refExpenses()).$asArray();
+    $scope.roomies = $firebase(fireBaseData.refRoomMates()).$asArray();
+    $scope.roomies.$loaded().then(function(array) {
+        //array = [[set1_rm1_email, set1_rm2_email], [set2_rm1_email, set2_rm2_email] ...]
+        for (var i = 0; i < array.length; i++) {
+            if (array[i][0] === $scope.user.password.email) {
+                $scope.roomiesEmail = array[i][1];
+            } else if (array[i][1] === $scope.user.password.email) {
+                $scope.roomiesEmail = array[i][0];
+            }
+        }
+        console.log($scope.roomiesEmail);
+        $scope.$apply();
+        // NOTE: For simplicity, this demo only supports the 2-roommate use case
+    });
 
-    $scope.expenses = [{
-        by: 'me',
-        label: 'test',
-        cost: 10
-    }];
-
+    $scope.addExpense = function(e) {
+        $scope.expenses.$add({
+            by: $scope.roomiesEmail ,
+            label: $scope.label,
+            cost: $scope.cost
+        });
+        $scope.label = "";
+        $scope.cost = 0;
+    };
     $scope.getTotal = function() {
         var rtnTotal = 0;
         for (var i = 0; i < $scope.expenses.length; i++) {
@@ -43,8 +59,36 @@ angular.module('starter.controllers', [])
     };
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('AccountCtrl', function($scope,fireBaseData) {
+
+  $scope.showLogin = false;
+  $scope.user = fireBaseData.ref().getAuth();
+  if(!$scope.user){
+      $scope.showLogin = true;
+  }
+
+  $scope.login = function (em,pwd) {
+    fireBaseData.ref().authWithPassword({
+      email: em,
+      password: pwd
+    },function (err,authData) {
+      if(err){
+        console.log('cannot authenticate user '+err);
+      }
+      else
+      {
+        console.log("User ID: " + authData.uid +", Provider: " + authData.provider);
+        $scope.user = fireBaseData.ref().getAuth();
+        $scope.showLogin = false;
+        $scope.$apply();
+      }
+    })
+  }
+
+
+    $scope.logout = function () {
+        fireBaseData.ref().unauth();
+        $scope.showLogin = true;
+    };
+
 });
